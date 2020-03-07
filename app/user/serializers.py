70d35beed59fa2285707e8e52ffa6d30226a5fbd
Milos Sehic <milos.sehic@miticon.com>
2020-03-07 02:26:49 +0100

    Managing and authentication Users

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -27,7 +27,6 @@ class UserSerializer(serializers.ModelSerializer):
         return user
 
 
-
 class AuthTokenSerializer(serializers.Serializer):
     """Serializer for authentication object"""
     email = serializers.CharField()
