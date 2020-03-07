3d06f03a3fbb5456c86b5c2f8a9af49a37e9654f
Milos Sehic <milos.sehic@miticon.com>
2020-03-08 00:43:36 +0100

    Added filtering and image uploading

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -16,7 +16,15 @@ class BaseRecipeAttrViewSet(viewsets.GenericViewSet,
 
     def get_queryset(self):
         """Filter for objects that only correspond to authenticated user"""
-        return self.queryset.filter(user=self.request.user).order_by('-name')
+        assigned_only = bool(
+            int(self.request.query_params.get('assigned_only', 0))
+        )
+        queryset = self.queryset
+        if assigned_only:
+            queryset = queryset.filter(recipe__isnull=False)
+
+        return queryset.filter(user=self.request.user)\
+            .order_by('-name').distinct()
 
     def perform_create(self, serializer):
         """Creating new object"""
@@ -35,6 +43,11 @@ class IngredientViewSet(BaseRecipeAttrViewSet):
     queryset = Ingredient.objects.all()
 
 
+def _params_to_ints(qs):
+    """Convert a list of string IDs to a list of integers"""
+    return [int(str_id) for str_id in qs.split(',')]
+
+
 class RecipeViewSet(viewsets.ModelViewSet):
     """List view (View set) for recipe"""
     serializer_class = serializers.RecipeSerializer
@@ -45,7 +58,16 @@ class RecipeViewSet(viewsets.ModelViewSet):
     def get_queryset(self):
         """Filter for recipes that belong only
         to this authenticated user"""
-        return self.queryset.filter(user=self.request.user).order_by('-id')
+        tags = self.request.query_params.get('tags')
+        ingredients = self.request.query_params.get('ingredients')
+        queryset = self.queryset
+        if tags:
+            tag_ids = _params_to_ints(tags)
+            queryset = queryset.filter(tags__id__in=tag_ids)
+        if ingredients:
+            ingredient_ids = _params_to_ints(ingredients)
+            queryset = queryset.filter(ingredients__id__in=ingredient_ids)
+        return queryset.filter(user=self.request.user).order_by('-id')
 
     def get_serializer_class(self):
         """Get appropriate serializer recipe object"""
